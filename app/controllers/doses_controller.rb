1f72ebee7a10e7948f554d7b84e462e086d7ec09
Victor Bianchi <vbianchi90@gmail.com>
2018-02-22 15:25:07 -0500

    implemented routes, controllers, and started working on front-end

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -17,6 +17,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
